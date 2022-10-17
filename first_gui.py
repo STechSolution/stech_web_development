9612121d56afbc4f8bc00f530af3244166941ce5
ghztcoder404 <ghztcoddertech@protonmail.com>
2022-10-16 18:03:38 -0700

    Fixed a typo

--- first_gui.py
+++ first_gui.py
@@ -2,8 +2,10 @@
 
 from guizero import *
 
+
 def error_handler():
-    info("Interface, ", "Press this button I dare you!")
+    info("Interface, ", "Press this button, I dare you!")
+
 
 app = App(title="Snider Technical Solutions", height=200)
 button = PushButton(app, text="Execute", command=error_handler)
